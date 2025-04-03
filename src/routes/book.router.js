188775a3b9358a4dd549acfac70a38399e6de175
dufefai <nvdu20@gmail.com>
2025-04-03 20:46:49 +0700

    Use Authorization in CRUD category, book and fix edit category

--- src/routes/book.router.js
+++ src/routes/book.router.js
@@ -1,12 +1,13 @@
 import bookController from "../controllers/bookController.js"
 import express from "express";
+import { verifyAdmin } from '../middlewares/JWT.js'
 
 const router = express.Router();
 
 
-router.post("/create", bookController.createBook);
-router.put("/edit/:id", bookController.editBook);
+router.post("/create", verifyAdmin, bookController.createBook);
+router.put("/edit/:id", verifyAdmin,  bookController.editBook);
 router.get("/getBooks", bookController.getBooks);
-router.delete("/delete/:id", bookController.deleteBook);
+router.delete("/delete/:id",verifyAdmin, bookController.deleteBook);
 
 export default router;
